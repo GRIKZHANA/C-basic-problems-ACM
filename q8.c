5a53b00b79b2651887acf31d25ff849d3c1374ef
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-09-27 13:00:40 +0530

    Add files via upload

---
+++ q8.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    float celcius =37.8;
+    float fahrenhiet = (celcius *9/5) +32;
+    printf("The temperature of celcius in fahrenheit is :%f",fahrenhiet);
+    return 0;
+
+}
