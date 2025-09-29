c3b0e73daf0a6246b578393071f352b76e58fa73
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-09-29 23:09:31 +0530

    Add files via upload

---
+++ q16.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=5;
+    if(a>b){
+        printf("The larger number is :%d\n",a);
+    }
+    
+        else{ printf("The larger number is :%d\n",b);
+        } 
+    return 0;
+
+}
