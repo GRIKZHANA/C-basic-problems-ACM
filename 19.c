53dfd29472fb4eb1ca6aca95f08fcf39be3dc902
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-10-01 23:12:09 +0530

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 20, b = 6;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("Addition (a + b) = %d\n", a + b);
+    printf("Subtraction (a - b) = %d\n", a - b);
+    printf("Multiplication (a * b) = %d\n", a * b);
+    printf("Division (a / b) = %d\n", a / b);   // integer division
+    printf("Modulus (a %% b) = %d\n", a % b);  // remainder
+
+    return 0;
+}
