31419461ff5bc67daf361f5450db05f824221c25
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-10-21 18:52:39 +0530

    Add files via upload

---
+++ q30.c
@@ -0,0 +1,43 @@
+
+    #include <stdio.h>
+int main() {
+    double num1, num2, result;
+    char operator;
+
+    // Input two numbers and operator
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);  // Space before %c to avoid newline issue
+
+    // Perform the calculation based on the operator
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
+
