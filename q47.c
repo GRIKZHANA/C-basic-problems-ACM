3f8d46a72ccdfe1d39fc82f3e2146406306049e3
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-10-24 15:12:02 +0530

    Add files via upload

---
+++ q47.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int base, exponent;
+    long long result = 1;
+
+    printf("Enter base: ");
+    scanf("%d", &base);
+
+    printf("Enter exponent: ");
+    scanf("%d", &exponent);
+
+    // Multiply base exponent times
+    for (int i = 1; i <= exponent; ++i) {
+        result *= base;
+    }
+
+    printf("%d^%d = %lld\n", base, exponent, result);
+
+    return 0;
+}
