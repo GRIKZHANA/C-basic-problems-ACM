bd64075934baf5cf567f1026a2446f93cd7f95fb
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-18 23:23:00 +0530

    Add files via upload

---
+++ Question133.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    gets(s);
+
+    int vowels = 0, consonants = 0;
+    for(int i = 0; s[i] != '\0'; i++){
+        char ch = s[i];
+        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+               ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    printf("Vowels: %d\nConsonants: %d", vowels, consonants);
+    return 0;
+}
