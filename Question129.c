e6e21196eb0317907e5963870cae816b84dc5241
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-18 23:22:38 +0530

    Add files via upload

---
+++ Question129.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    gets(str);
+
+    int n = strlen(str);
+
+    for(int i = 0, j = n-1; i < j; i++, j--){
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
