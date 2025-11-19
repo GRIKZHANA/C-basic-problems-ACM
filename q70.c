89c72034318f024cb4e3b34413eaa101a1538ff0
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-19 22:04:03 +0530

    Add files via upload

---
+++ q70.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int max(int arr[], int n) {
+    int largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];
+    }
+    return largest;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("%d", max(arr, n));
+
+    return 0;
+}
