44c992e2fc340fbfe038bb62807379fae4093ad4
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-20 23:15:44 +0530

    Add files via upload

---
+++ q74.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char s[200];
+    fgets(s, sizeof(s), stdin);
+
+    for(int i=0; s[i]; i++)
+        if(s[i] >= 'a' && s[i] <= 'z')
+            s[i] -= 32;
+
+    printf("%s", s);
+    return 0;
+}
