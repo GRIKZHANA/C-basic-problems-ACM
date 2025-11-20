a6d38528d646b6e9a4a1e1bbe1d65e7387aa26f9
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-20 23:15:11 +0530

    Add files via upload

---
+++ q72.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int min1 = INT_MAX, min2 = INT_MAX;
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] < min1){
+            min2 = min1;
+            min1 = arr[i];
+        } 
+        else if(arr[i] < min2 && arr[i] != min1){
+            min2 = arr[i];
+        }
+    }
+
+    if (min2 == INT_MAX)
+        printf("No second smallest");
+    else
+        printf("%d", min2);
+
+    return 0;
+}
