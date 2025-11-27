23e865071380a864649cea0d5d798892a90ca957
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-27 16:00:06 +0530

    Add files via upload

---
+++ q103.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    int count = 0;
+
+    scanf("%s", s);
+
+    for(int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+           c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+            count++;
+    }
+
+    printf("%d", count);
+    return 0;
+}
